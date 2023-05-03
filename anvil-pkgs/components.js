8f8e7a4596debfc907297084bae2a67f27cc3c4c
Uffe Arlo <uat@aperitas.com>
2023-05-03 14:17:05 +0200

    v.0.2.1

--- anvil-pkgs/components.js
+++ anvil-pkgs/components.js
@@ -3,4 +3,4 @@ import * as _form from "../components/components/form.js";
 import * as _nav from "../components/components/nav.js";
 import * as _navbar from "../components/components/navbar.js";
 import * as _textInput from "../components/components/text-input.js";
-import * as _toast from "../components/components/toast.js";
+export { show as show_toast } from "../components/components/toast.js";
