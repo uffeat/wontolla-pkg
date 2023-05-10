7a8e2e0790e69a0f021eb15819b7d9d676e7515d
Uffe Arlo <uat@aperitas.com>
2023-05-10 21:54:20 +0200

    k

--- main.js
+++ main.js
@@ -7,45 +7,44 @@ import * as _routes from "./router/routes.js";
 
 import { modal } from "./components/components/modal.js";
 
-const call = async (name, callback, q) => {
+const call = async (name, kwargs, callback) => {
   const request = new Request(
-    `https://wontolla-client.anvil.app/_/api/post/${name}`,
+    `https://wontolla-client.anvil.app/_/api/call/${name}`,
     {
       method: "POST",
-      body: JSON.stringify(q),
+      body: JSON.stringify(kwargs),
     }
   );
 
   const response = await fetch(request);
   //console.log(response);
 
-  const contentType = response.headers.get("Content-Type");
+  if (response.ok) {
+    const contentType = response.headers.get("Content-Type");
 
-  if (contentType.includes("application/json")) {
-    const result = await response.json();
-    callback && callback(result);
-    return result;
-  } else if (contentType.includes("text/plain")) {
-    const result = await response.text();
-
-    try {
-      const resultJson = JSON.parse(result);
-      callback && callback(resultJson);
-      return resultJson;
-    } catch {
+    if (contentType.includes("application/json")) {
+      const result = await response.json();
       callback && callback(result);
       return result;
+    } else if (contentType.includes("text/plain")) {
+      const result = await response.text();
+
+      try {
+        const resultJson = JSON.parse(result);
+        callback && callback(resultJson);
+        return resultJson;
+      } catch {
+        callback && callback(result);
+        return result;
+      }
     }
-  } else {
-    callback && callback(400);
-    return 400;
   }
 };
 
-const result = await call("test");
-console.log(`Result: ${JSON.stringify(result)}`)
+const result = await call("test", {'email':"uffeat@gmail.com"});
+console.log(`Result: ${JSON.stringify(result)}`);
 
-call("test", (result) =>
+call("test", {'email':"uffeat@gmail.com"}, (result) =>
   console.log(`Callback got result: ${JSON.stringify(result)}`)
 );
 
